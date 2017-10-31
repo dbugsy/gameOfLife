77ede9c1eedea9786ed9bdee39831dc7ab0b3cd4
dbugsy <dpbergquist@gmail.com>
2017-10-31 21:05:45 +0000

    refactor extract method coordinate._validate

--- src/Coordinate.ts
+++ src/Coordinate.ts
@@ -12,10 +12,13 @@ export default class Coordinate {
   }
 
   public next(gridSize: number): Coordinate {
-    if (gridSize < 1) { throw new Error("gridSize must be greater than 0");}
-    if (this.x === gridSize && this.y === gridSize) { throw new Error("Coordinate is beyond grid"); }
+    this._validate(gridSize);
     if (this.y === gridSize) { return new Coordinate(this.x + 1, 1); }
     return new Coordinate(1, this.y + 1);
   }
 
+  private _validate(gridSize: number): void {
+    if (gridSize < 1) { throw new Error("gridSize must be greater than 0"); }
+    if (this.x === gridSize && this.y === gridSize) { throw new Error("Coordinate is beyond grid"); }
+  }
 }
