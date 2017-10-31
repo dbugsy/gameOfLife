a44223d69bf87ef4be2627560af3abb6b265b8d8
dbugsy <dpbergquist@gmail.com>
2017-10-31 17:53:56 +0000

    single cell will not survive a step

--- src/Cell.ts
+++ src/Cell.ts
@@ -1,9 +1,24 @@
 import { LifeStatus } from "./LifeStatus";
 
 export default class Cell {
-  public at(position: number): Cell {
+  private currentLifeStatus: LifeStatus;
+
+  constructor(gridSize: number = 1) {
+    this.currentLifeStatus = LifeStatus.DEAD;
+  }
+
+  public populate(position: number): void {
+    this.currentLifeStatus = LifeStatus.ALIVE;
+  }
+
+  public lifeStatus(position: number): LifeStatus {
+    return this.currentLifeStatus;
+  }
+
+  public step(): Cell {
     return new Cell();
   }
+
   public nextRoundStatus(): LifeStatus {
     return LifeStatus.DEAD;
   }
--- test/Cell.test.ts
+++ test/Cell.test.ts
@@ -6,13 +6,26 @@ describe("Cell", () => {
 
   describe("will form a grid,", () => {
     describe("with a default size of 1 * 1", () => {
-      it("will have a cell at position 1", () => {
+      it("will have a dead cell at position 1", () => {
         const cell = new Cell();
-        expect(cell.at(1)).toBeInstanceOf(Cell);
+        expect(cell.lifeStatus(1)).toBe(LifeStatus.DEAD);
       });
+
+      it("can be populated", () => {
+        const cell = new Cell();
+        cell.populate(1);
+        expect(cell.lifeStatus(1)).toBe(LifeStatus.ALIVE);
+      });
+
+      it("will not survive a step", () => {
+        const cell = new Cell();
+        cell.populate(1);
+        const nextStep = cell.step();
+        expect(nextStep.lifeStatus(1)).toBe(LifeStatus.DEAD);
+      });
+
     });
   });
-
 
   describe("will not survive", () => {
     it("with no neighbours", () => {
