b5b9db00fd9fc336f2731a58e329b432baaf1247
dbugsy <dpbergquist@gmail.com>
2017-10-31 17:28:14 +0000

    Will have a cell at position 1

--- src/Cell.ts
+++ src/Cell.ts
@@ -1,6 +1,9 @@
 import { LifeStatus } from "./LifeStatus";
 
 export default class Cell {
+  public at(position: number): Cell {
+    return new Cell();
+  }
   public nextRoundStatus(): LifeStatus {
     return LifeStatus.DEAD;
   }
--- test/Cell.test.ts
+++ test/Cell.test.ts
@@ -4,6 +4,16 @@ import { LifeStatus } from "../src/LifeStatus";
 
 describe("Cell", () => {
 
+  describe("will form a grid,", () => {
+    describe("with a default size of 1 * 1", () => {
+      it("will have a cell at position 1", () => {
+        const cell = new Cell();
+        expect(cell.at(1)).toBeInstanceOf(Cell);
+      });
+    });
+  });
+
+
   describe("will not survive", () => {
     it("with no neighbours", () => {
       const cell = new Cell();
